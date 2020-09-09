3087bcd93b8bc06cdd82bfaa11ddfbe789fd4585
AK <icecicle04@gmail.com>
2020-09-09 19:22:51 -0400

    added questions and answers for the quiz

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
 console.log("hello World!");
+// insert questions array
+var quizQuestions = [
+  {
+    0: "Commonly used data types DO NOT include:",
+    1: "1. strings",
+    2: "2. booleans",
+    3: "3. alerts",
+    4: "4. numbers",
+    correct: "3",
+  },
+  {
+    0: "The condition in an if / else statement is enclosed within ______.",
+    1: "1. quotes",
+    2: "2. curley brackets",
+    3: "3. parenthesis",
+    4: "4. numbers",
+    correct: "3",
+  },
+  {
+    0: "Arrays in JavaScript can be used to store ______.",
+    1: "1. numbers and strings",
+    2: "2. other arrays",
+    3: "3. booleans",
+    4: "4. all of the above",
+    correct: "4",
+  },
+  {
+    0: "String values must be enclosed within ______ when being assigned to variables.",
+    1: "1. commas",
+    2: "2. curley brackets",
+    3: "3. quotes",
+    4: "4. parenthesis",
+    correct: "3",
+  },
+  {
+    0: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    1: "1. JavaScript",
+    2: "2. terminal / bash",
+    3: "3. for loops",
+    4: "4. console log",
+    correct: "4",
+  },
+];
+
+console.log(quizQuestions);
